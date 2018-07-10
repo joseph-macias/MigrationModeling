aa2f6e0cc0aa0443af44d352a4e44d037f39e091
Unknown <joanna.a.bieri@gmail.com>
2018-07-10 11:32:37 -0700

    Updating the code to match Joanna's local directory 7/10/18

--- CodeMonarchs_NewEqn/MonarchSimulation.R
+++ CodeMonarchs_NewEqn/MonarchSimulation.R
@@ -3,20 +3,21 @@
 ##################
 library(XLConnect)
 
+
 #########################################
-### SET SPECIES SPECIFIC NETWORK INFO ###
+### SET SPECEIS SPECIFIC NETWORK INFO ###
 #########################################
 
 ### MONARCH EXAMPLE ###
 
 # IMPORTANT NOTE #
 # Users should specify network model functions f_(i,t), p_(ij,t), and s_(ij,t) in the file:
-#           NetworkFunctions.R, even if these are constants they must still be set as such.
+# NetworkFunctions.R, even if these are constants they must still be set as such.
 
 ### User specified information specific to the simulation ###
 
 SIMNAME <- "Baseline1" # Specifies which baseline folder 
-# This is the subfolder where the network data, outputs, and .RData file should are saved
+# This is the subfoulder where the network data, outputs, and .RData file should are saved
 # This is also the subfoulder where the network input files are stored: 
                         #   "./SIMNAME/network_inputs_NETNAME.xlsx
 
@@ -29,7 +30,6 @@ ERR <- .01 # Error tolerance for convergence.
 
 seasons <- 7 # Number of seasons or steps in one annul cycle. 
 # This must match number of spreadsheets in input files
-
 num_nodes <- 4 # Number of nodes in the network
 # This must match the number of initial conditions given in input files
 
@@ -37,12 +37,13 @@ tmax <- 201 # Maximum number of steps to take - assume non convergence if t=tmax
 
 OUTPUTS <- TRUE # TRUE = Process final outputs, FALSE = Do not process just run the sumulation.
 
+delta <- 1 # Used in KR Calculation 1 = full node removal 0 = do nothing
+
 ## For debugging your model equations ##
 SILENT <- TRUE # TRUE = Do not print data to console - silence outputs.
                 # FALSE = Print population data and network function data to the Console for debugging.
-SAVE_VAR <- TRUE # TRUE = save f_update, p_update, s_update, and CR for each time step of the calculation.
-                    # This could lead to an extremely large data set!!!
-                 # FALSE = Do not save this data, just report CR at the end.
+SAVE_VAR <- TRUE
+
 
 
 
@@ -59,13 +60,12 @@ setwd(this.dir)
 # Set location of source code
 netcode <- c("../NetworkCode1.2_NewEqn/")
 # Clear the workspace reserving needed network input variables
-base_variables <- c("seasons", "num_nodes", "NETNAME", "tmax", "SIMNAME", "ERR", "OUTPUTS", "SILENT","netcode","base_variables","SAVE_VAR")
-base_variables<- c(base_variables, "pert_variables")
+base_variables <- c("seasons", "num_nodes", "NETNAME", "tmax", "SIMNAME", "ERR", "OUTPUTS", "SILENT","netcode","base_variables", "SAVE_VAR", "delta")
 if(!exists("pert_variables")){pert_variables <- c("pert_variables")}
-rm(list=setdiff(ls(), c(base_variables, pert_variables)))
-
+base_variables <- c(base_variables, "pert_variables")
+rm(list=setdiff(ls(), base_variables))
 if(SAVE_VAR == TRUE){
-  save_variables <- c("save_variables")
+  save_variables <- c("save_varibles")
   base_variables <- c(base_variables, "save_variables")   }
 
 ### SET UP THE NETWORK(S) ###
@@ -82,6 +82,7 @@ source(paste(netcode,"NetworkSimulation.R",sep=""))
 if (OUTPUTS == T){
   source(paste(netcode,"NetworkOutputs.R",sep=""))
 }
+
 
 ######################
 ### SAVE THE DATA ####
--- CodePlants_NewEqn/PlantsSimulation.R
+++ CodePlants_NewEqn/PlantsSimulation.R
@@ -3,22 +3,23 @@
 ##################
 library(XLConnect)
 
+
 #########################################
-### SET SPECIES SPECIFIC NETWORK INFO ###
+### SET SPECEIS SPECIFIC NETWORK INFO ###
 #########################################
 
-### PLANT EXAMPLE ###
+### PLANTS EXAMPLE ###
 
 # IMPORTANT NOTE #
 # Users should specify network model functions f_(i,t), p_(ij,t), and s_(ij,t) in the file:
-#           NetworkFunctions.R, even if these are constants they must still be set as such.
+# NetworkFunctions.R, even if these are constants they must still be set as such.
 
 ### User specified information specific to the simulation ###
 
 SIMNAME <- "Baseline1" # Specifies which baseline folder 
-# This is the subfolder where the network data, outputs, and .RData file should are saved
+# This is the subfoulder where the network data, outputs, and .RData file should are saved
 # This is also the subfoulder where the network input files are stored: 
-                        #   "./SIMNAME/network_inputs_NETNAME.xlsx
+#   "./SIMNAME/network_inputs_NETNAME.xlsx
 
 NETNAME <- c("seeds","plants") # Give a distinct name for each class as used in input files
 # Order is important here we would index [[1]] = class 1 and [[2]] = class 2 in alpha and beta.
@@ -37,12 +38,12 @@ tmax <- 201 # Maximum number of steps to take - assume non convergence if t=tmax
 
 OUTPUTS <- TRUE # TRUE = Process final outputs, FALSE = Do not process just run the sumulation.
 
+delta <- 1 # Used in KR Calculation 1 = full node removal 0 = do nothing
+
 ## For debugging your model equations ##
 SILENT <- TRUE # TRUE = Do not print data to console - silence outputs.
-                # FALSE = Print population data and network function data to the Console for debugging.
-SAVE_VAR <- TRUE # TRUE = save f_update, p_update, s_update, and CR for each time step of the calculation.
-                    # This could lead to an extremely large data set!!!
-                 # FALSE = Do not save this data, just report CR at the end.
+# FALSE = Print population data and network function data to the Console for debugging.
+SAVE_VAR <- TRUE
 
 
 
@@ -58,14 +59,15 @@ setwd(this.dir)
 
 # Set location of source code
 netcode <- c("../NetworkCode1.2_NewEqn/")
-# Clear the workspace reserving needed network input variables
-base_variables <- c("seasons", "num_nodes", "NETNAME", "tmax", "SIMNAME", "ERR", "OUTPUTS", "SILENT","netcode","base_variables","SAVE_VAR")
-base_variables<- c(base_variables, "pert_variables")
-if(!exists("pert_variables")){pert_variables <- c("pert_variables")}
-rm(list=setdiff(ls(), c(base_variables, pert_variables)))
 
+# Clear the workspace reserving needed network input variables
+base_variables <- c("seasons", "num_nodes", "NETNAME", "tmax", "SIMNAME", "ERR", "OUTPUTS", "SILENT","netcode","base_variables","SAVE_VAR", "delta")
+if(!exists("pert_variables")){pert_variables <- c("pert_variables")}
+base_variables <- c(base_variables, "pert_variables")
+rm(list=setdiff(ls(), base_variables))
+SAVE_VAR <- TRUE
 if(SAVE_VAR == TRUE){
-  save_variables <- c("save_variables")
+  save_variables <- c("save_varibles")
   base_variables <- c(base_variables, "save_variables")   }
 
 ### SET UP THE NETWORK(S) ###
@@ -82,6 +84,7 @@ source(paste(netcode,"NetworkSimulation.R",sep=""))
 if (OUTPUTS == T){
   source(paste(netcode,"NetworkOutputs.R",sep=""))
 }
+
 
 ######################
 ### SAVE THE DATA ####
